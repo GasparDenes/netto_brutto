401af992c8358255f30db42ee7dc038b69cf130c
Dénes Gáspár <gdenes007@gmail.com>
2026-03-29 00:32:08 +0100

    Solve task and add gitignore

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    static List<double> brutto = new List<double>();
+    static void Main(string[] args)
+    {
+        StreamReader sr= new StreamReader("netto.txt");
+        while (!sr.EndOfStream)
+        {
+            double novelt_ar=Math.Round((Convert.ToDouble(int.Parse(sr.ReadLine())*1.27)), 0);
+            brutto.Add(novelt_ar);
+        }
+        sr.Close();
+        StreamWriter sw = new StreamWriter("brutto.txt");
+        foreach (var item in brutto)
+        {
+            sw.WriteLine(item);
+        }
+        sw.Close();
+    }
+}
